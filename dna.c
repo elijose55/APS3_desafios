b1f2f07b247ac9035e63b70922d350de787238fc
eli <elijose55@hotmail.com>
2019-03-25 20:37:54 -0300

    parte 1 pronta

--- dna.c
+++ dna.c
@@ -19,44 +19,29 @@ void load_string(FILE *file, int *p, char *s, int t) {
 
 int mlcs_w(char a[], int n, char b[], int m, int length[MAX_SIZE + 1][MAX_SIZE + 1]) {
   if (length[n][m] != -1){
-    printf("e ");
     return length[n][m];
   }
   if (m == 0 || n == 0){
     length[n][m] = 0;
-    printf("z ");
+    length[0][0] = 0;
     return 0;
   }
-  printf("%d ", a[n]);
-  printf("%d ", b[m]);
-  printf("%d ", m);
-  printf("%d ", n);
-  printf("%d ", b[m-1] == a[n-1]);
+
+  mlcs_w(a, n-1, b, m, length);
+  mlcs_w(a, n, b, m-1, length);
+
   if(a[n-1] == b[m-1]){
-    printf("q11 ");
-    length[n][m] = mlcs_w(a, n-1, b, m-1, length) + 1;
-    printf("q2 ");
-    return length[n][m];
+    length[n][m] = length[n-1][m-1] + 1;
   }
   else{
-    if (length[n-1][m] == -1){
-      printf("n ");
-      length[n-1][m] = mlcs_w(a, n-1, b, m, length);
-    }
-    if (length[n][m-1] == -1){
-      printf("m ");
-      length[n][m-1] = mlcs_w(a, n, b, m-1, length);
-    }
-    
     if (length[n-1][m] > length[n][m-1]){
-      printf("rn ");
-      return length[n-1][m];
+      length[n][m] = length[n-1][m];
     }
     else{
-      printf("rm ");
-      return length[n][m-1];
+      length[n][m] = length[n][m-1];
     }
   }
+  return length[n][m];
 }
 
 int mlcs(char a[], int n, char b[], int m) {
@@ -67,9 +52,8 @@ int mlcs(char a[], int n, char b[], int m) {
     length[s][i] = -1;
   }
   }
-  //mlcs_w(a, n, b, m, length);
-
-  return mlcs_w(a, n, b, m, length);
+  mlcs_w(a, n, b, m, length);
+  return length[n][m];
 }
 
 int dlcs(char a[], int n, char b[], int m) {
