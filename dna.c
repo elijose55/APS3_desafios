05a974ccfb0dad9ca17b47e85aad721a2f673d07
pedrooa <pedrooa@al.insper.edu.br>
2019-03-26 12:27:39 -0300

    Integration test funcionando

--- dna.c
+++ dna.c
@@ -18,27 +18,24 @@ void load_string(FILE *file, int *p, char *s, int t) {
 }
 
 int mlcs_w(char a[], int n, char b[], int m, int length[MAX_SIZE + 1][MAX_SIZE + 1]) {
-  if (length[n][m] != -1){
+  if (length[n][m] != -1) {
     return length[n][m];
   }
-  if (m == 0 || n == 0){
+  if (m == 0 || n == 0) {
     length[n][m] = 0;
-    length[0][0] = 0;
     return 0;
   }
 
-  mlcs_w(a, n-1, b, m, length);
-  mlcs_w(a, n, b, m-1, length);
+  mlcs_w(a, n - 1, b, m, length);
+  mlcs_w(a, n, b, m - 1, length);
 
-  if(a[n-1] == b[m-1]){
-    length[n][m] = length[n-1][m-1] + 1;
-  }
-  else{
-    if (length[n-1][m] > length[n][m-1]){
-      length[n][m] = length[n-1][m];
-    }
-    else{
-      length[n][m] = length[n][m-1];
+  if (a[n - 1] == b[m - 1]) {
+    length[n][m] = length[n - 1][m - 1] + 1;
+  } else {
+    if (length[n - 1][m] > length[n][m - 1]) {
+      length[n][m] = length[n - 1][m];
+    } else {
+      length[n][m] = length[n][m - 1];
     }
   }
   return length[n][m];
@@ -47,15 +44,32 @@ int mlcs_w(char a[], int n, char b[], int m, int length[MAX_SIZE + 1][MAX_SIZE +
 int mlcs(char a[], int n, char b[], int m) {
   int length[MAX_SIZE + 1][MAX_SIZE + 1];
 
-  for(int i = 0; i < MAX_SIZE + 1; i++){
-    for(int s = 0; s < MAX_SIZE + 1; s++){
-    length[s][i] = -1;
+  for (int i = 0; i < MAX_SIZE + 1; i++) {
+    for (int s = 0; s < MAX_SIZE + 1; s++) {
+      length[s][i] = -1;
+    }
   }
-  }
+  length[0][0] = 0;
   mlcs_w(a, n, b, m, length);
   return length[n][m];
 }
 
 int dlcs(char a[], int n, char b[], int m) {
-  return 2;
+  int length[MAX_SIZE + 1][MAX_SIZE + 1];
+  for (int i = 0; i <= n; i++) {
+    for (int j = 0; j <= m; j++) {
+      if (i == 0 || j == 0) {
+        length[i][j] = 0;
+      } else if (a[i - 1] == b[j - 1]) {
+        length[i][j] = length[i - 1][j - 1] + 1;
+      } else {
+        if (length[i - 1][j] > length[i][j - 1]) {
+          length[i][j] = length[i - 1][j];
+        } else {
+          length[i][j] = length[i][j - 1];
+        }
+      }
+    }
+  }
+  return length[n][m];
 }
